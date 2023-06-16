6263c2253e7350cd9ea4240399deb3f0d2009833
Kyym <kevinkimani165@gmail.com>
2023-06-15 17:03:40 -0700

    root@a7ab304e4f95:~/alx-low_level_programming/0x00-hello_world# gcc 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <-stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle"
+ * main - Entry point
  * 
- * Return 0
+ * Return Always 0 (Success)
  */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
+   puts("Programming is like building a multilingual puzzle\n");
    return (0);
 }
 
